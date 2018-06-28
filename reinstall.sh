d1649617323747bc3a591b63f3a9ae341bb462dc
PerfectStorm85 <info@ronkamphuis.nl>
2018-06-28 18:15:58 +0000

    First commit

---
+++ reinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cat ./logo
+echo
+echo "You are trying to (re)install the Rock64 Full Iota node,"
+echo "created by https://www.Rock64Iota.com"
+echo
+echo
+echo "If you bought the node before, and are now running the reinstall script,"
+echo "please email me your full name and I will make sure your node gets reinstalled."
+echo
+echo "Kind regards,"
+echo "Rock64Iota.com"
+echo
+exit
